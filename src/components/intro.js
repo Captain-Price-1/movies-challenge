ee6ce1cf76a1a0040ab27a9282e055d6b6d0e34c
Captain-Price <94977890+Captain-Price-1@users.noreply.github.com>
2022-08-17 22:59:00 +0530

    initial

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,6 +3,9 @@ import ServiceProvider from "./serviceProvider";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 const intro = () => {
+  let a = 6;
+  let b = 55;
+  let c = 67;
   return (
     <article className="intro-main">
       <div className="intro-screen">
